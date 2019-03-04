83894a5c99f65b7a026b2060804000816febd34f
jnoxro <jackorton32@gmail.com>
2019-03-04 01:23:24 +0000

    Working on joysick stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+extern crate stick;
+
+use std::io::stdin;
+//use stick::Input::Move;
+
 fn main() {
+
     println!("Hello, world!");
+    let stdin = stdin();
+    let mut cm = stick::ControllerManager::new(vec![]);
+//    let mut test1 = 0.00;
+//    let mut test2 = 0.00;
+    'a: loop {
+        while let Some((j, i)) = cm.update(){
+
+	println!("{}: {}", j, i);
+//            match i {
+//                        Move(x, y) => {
+//			    test1 = y;
+//			    test2 = x;
+//                        }
+//            }
+        }
+//    println!("x: {}, y: {}", test1, test2);
+    }
+
 }
